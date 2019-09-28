9cb4dda9fe99f352c7c576191b854b67210f9ffa
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-09-28 16:36:19 -0300

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const routes = require("./routes")
+
+const server = express()
+
+mongoose.connect("mongodb+srv://tindev:tindev@tindev-uer41.mongodb.net/tindev?retryWrites=true&w=majority", {
+  useNewUrlParser: true
+})
+
+server.use(express.json())
+server.use(cors())
+server.use(routes)
+
+server.listen(3333)
