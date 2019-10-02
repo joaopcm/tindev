bce01b8edc9362c0436cc3b2218a799a515d6f80
João Pedro da Cruz Melo <jopcmelo@gmail.com>
2019-10-01 21:00:52 -0300

    Finaliza o projeto com a funcionalidade de realtime com matches

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,34 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const routes = require("./routes")
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-const server = express()
+const routes = require('./routes')
 
-mongoose.connect("mongodb+srv://tindev:tindev@tindev-uer41.mongodb.net/tindev?retryWrites=true&w=majority", {
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query
+  connectedUsers[user] = socket.id
+})
+
+mongoose.connect('mongodb+srv://tindev:tindev@tindev-uer41.mongodb.net/tindev?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-server.use(express.json())
-server.use(cors())
-server.use(routes)
+app.use((req, res, next) => {
+  req.io = io
+  req.connectedUsers = connectedUsers
+
+  return next()
+})
+
+app.use(express.json())
+app.use(cors())
+app.use(routes)
 
 server.listen(3333)
